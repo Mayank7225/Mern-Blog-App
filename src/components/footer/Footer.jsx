e1c648d08b8cf08b84fe1abc8293721a739a1bc6
mayank singh <astronomyclub@vnit.ac.in>
2024-01-26 23:37:43 +0530

    master bracnch

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import classes from './footer.module.css'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className={classes.wrapper}>
+        <div className={classes.col}>
+          <h2>About the App</h2>
+          <p>
+The MERN Blog App is a dynamic web platform powered by MongoDB, Express.js, React, and Node.js. Seamlessly integrating frontend and backend technologies, it enables users to create, edit, and share blog posts. With responsive design and user-friendly features, the app offers an engaging writing and reading experience. Leveraging the MERN stack's scalability and efficiency
+          </p>
+        </div>
+        <div className={classes.col}>
+          <h2>Contacts</h2>
+          <span>Phone: +91 7505709050</span>
+          <span>LinkedIn: Mayank Singh</span>
+          <span>GitHub: Mayank Singh</span>
+        </div>
+        <div className={classes.col}>
+          <h2>Location</h2>
+          <span>Continent: Asia</span>
+          <span>Country: India</span>
+          <span>Current India: Nagpur</span>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import classes from './home.module.css'
 import Navbar from '../../components/navbar/Navbar'
 import FeaturedBlogs from '../../components/featuredBlogs/FeaturedBlogs'
+import Categories from '../../components/categories/Categories'
+import Newsletter from '../../components/newsletter/Newsletter'
+import Footer from '../../components/footer/Footer'
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Categories/>
         <FeaturedBlogs/>
+        <Newsletter/>
+        <Footer/>
     </div>
   )
 }
